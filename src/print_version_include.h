a258ddee3f97b1b00e2660e92315b7143a02ffbf
Yang Wang's iMac 2018 <ywg@yangmac.psc.edu>
2018-07-27 10:17:54 -0400

    Fixed a minor bug in PotentialModule.F90

--- src/print_version_include.h
+++ src/print_version_include.h
@@ -4,4 +4,4 @@
 ! 
    write(myunit,'("# ",t5,a,t30,": ",a)') "username",trim("ywg")
    write(myunit,'("# ",t5,a,t30,": ",a)') "system name",trim("yangmac.psc.edu")
-   write(myunit,'("# ",t5,a,t30,": ",a)') "code compiled",trim("Friday, July 20 2018 16:28:20")
+   write(myunit,'("# ",t5,a,t30,": ",a)') "code compiled",trim("Thursday, July 26 2018 10:42:36")
